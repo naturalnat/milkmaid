0085c9be05b2699a8c2aa798bb03ffa6966e0c89
Natalie Taktachev <ntaktach@gmail.com>
2020-04-15 18:23:52 -0400

    milk class controller, redirect if not logged in

--- app/controllers/milks_controller.rb
+++ app/controllers/milks_controller.rb
@@ -1,4 +1,4 @@
 class MilksController < ApplicationController
-  has_many :reviews
-  has_many :users, through: :reviews #people who have reviewed
+  before_action :redirect_if_not_logged_in
+  
 end
